cfb88a9bf8d29dcaf6a5364abfa3f3aa27afa1e9
Alireza <alirezaheydari7909@gmail.com>
2024-09-10 15:13:36 +0330

    add time-conversion.js file

---
+++ algorithm/time-conversion.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+    // Write your code here
+    const time = s.split(':')
+    const amOrPm = time.pop().match(/.{0,2}/g)
+    if (amOrPm[1] == 'PM') {
+        const hour = (e) => {
+            if (e == '12') {
+                return e;
+            } else {
+                return parseInt(e) + 12
+            }
+        } 
+        
+        return hour(time[0]) + ':' + time[1] + ':' + amOrPm[0];
+    } else {
+        const hour = (e) => {
+            if (e == '12') {
+                return '00';
+            } else {
+                return e;
+            }
+        }
+        return hour(time[0]) + ':' + time[1] + ':' + amOrPm[0];
+    }
+}
+
+timeConversion('07:05:45PM')
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = timeConversion(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}

1514dd22be1e5c196a101f62273af4ece659f026
Alireza <alirezaheydari7909@gmail.com>
2024-09-16 18:04:02 +0330

    fix eslint bug in NarBar component

--- search/src/components/NavBar/index.jsx
+++ search/src/components/NavBar/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import './style.css';
+import React from 'react'
+import './style.css'
 
-export default function NavBar() {
+export default function NavBar () {
   return (
     <nav>
       NavBar

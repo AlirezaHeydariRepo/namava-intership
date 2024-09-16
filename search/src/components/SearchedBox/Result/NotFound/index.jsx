1e8d45f880d921b4c010101bdf741bebb94c91b4
Alireza <alirezaheydari7909@gmail.com>
2024-09-16 18:09:05 +0330

    add style and structure of NotFound component

---
+++ search/src/components/SearchedBox/Result/NotFound/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import notFoundImage from '../../../../assets/images/not-found.png'
+export default function NotFound () {
+  return (
+    <div className='not-found'>
+      <img className="no-found-image" src={notFoundImage} alt="not-found" />
+      <span className="no-found-text">موردی یافت نشد.</span>
+    </div>
+  )
+}

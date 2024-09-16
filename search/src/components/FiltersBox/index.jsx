e0b6dd8143ade8ae2ada4ec9e0f7bf4d99261905
Alireza <alirezaheydari7909@gmail.com>
2024-09-16 17:55:59 +0330

    fix infinite render

--- search/src/components/FiltersBox/index.jsx
+++ search/src/components/FiltersBox/index.jsx
@@ -1,24 +1,27 @@
-import React, { useState } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import './style.css'
-
-export default function FilterBox () {
-  const [type, setType] = useState('')
+FilterBox.propTypes = {
+  type: PropTypes.string,
+  setType: PropTypes.any
+}
+export default function FilterBox (props) {
   const isActive = (newType) => {
-    if (type === '') {
-      setType(newType)
-    } else if (type === newType) {
-      setType('')
+    if (props.type === '') {
+      props.setType(newType)
+    } else if (props.type === newType) {
+      props.setType('')
     } else if (newType === 'movie') {
-      if (type === 'all') {
-        setType('series')
+      if (props.type === 'all') {
+        props.setType('series')
       } else {
-        setType('all')
+        props.setType('all')
       }
     } else if (newType === 'series') {
-      if (type === 'all') {
-        setType('movie')
+      if (props.type === 'all') {
+        props.setType('movie')
       } else {
-        setType('all')
+        props.setType('all')
       }
     }
   }
@@ -27,17 +30,17 @@ export default function FilterBox () {
       <div className="filter-title">فیلترها</div>
       <div className="filters">
         <span
-          className={`container ${type === 'all' || type === 'movie' ? 'movie' : ''}`}
+          className={`container ${props.type === 'all' || props.type === 'movie' ? 'movie' : ''}`}
           onClick={() => isActive('movie')}>
           <span className="text">فیلم</span>
-          <input type="checkbox" checked={type === 'all' || type === 'movie' ? 'checked' : ''}/>
+          <input type="checkbox" checked={props.type === 'all' || props.type === 'movie' ? 'checked' : ''}/>
           <span className="checkmark"></span>
         </span>
         <span
-          className={`container ${type === 'all' || type === 'series' ? 'series' : ''}`}
+          className={`container ${props.type === 'all' || props.type === 'series' ? 'series' : ''}`}
           onClick={() => isActive('series')}>
           <span className="text">سریال</span>
-          <input type="checkbox" checked={type === 'all' || type === 'series' ? 'checked' : ''}/>
+          <input type="checkbox" checked={props.type === 'all' || props.type === 'series' ? 'checked' : ''}/>
           <span className="checkmark"></span>
         </span>
       </div>

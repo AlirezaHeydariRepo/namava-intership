9bbc3a060a21e504ae1d441e9c219f31fbcaa349
Alireza <alirezaheydari7909@gmail.com>
2024-09-16 18:08:11 +0330

    add style and structure of EmptyState

---
+++ search/src/components/SearchedBox/Result/EmptyState/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import emptyStateImage from '../../../../assets/images/empty-state.png'
+import './style.css'
+export default function EmptyState () {
+  return (
+    <div className='empty-state'>
+      <img className='empty-image' src={emptyStateImage} alt="empty-state" />
+      <span className='empty-text'>عنوان فیلم، سریال یا بازیگر مورد نظر خود را جستجو کنید و یا از طریق فیلتر‌های موجود، فیلم و سریال مورد علاقه خود را پیدا کنید. </span>
+    </div>
+  )
+}

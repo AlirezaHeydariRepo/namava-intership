fceacff85dd6a40f5b9fc34c782da55c7a5850fd
Alireza <alirezaheydari7909@gmail.com>
2024-09-16 18:14:51 +0330

    fetch data and pass them to components add necessary states

--- search/src/pages/searchPage/index.jsx
+++ search/src/pages/searchPage/index.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css'
 import InputBox from '../../components/InputBox'
-import SearchResult from '../../components/SearchedBox'
-import FilterBox from '../../components/FilterBox'
+import SearchedBox from '../../components/SearchedBox'
+import axios from 'axios'
+import FiltersBox from '../../components/FiltersBox'
 
 export default function SearchPage () {
+  const [type, setType] = useState('')
+  const [input, setInput] = useState('')
+  const [data, setData] = useState([])
+  const [view, setView] = useState('empty')
+  const [totalContent, setTotalContent] = useState('')
+  const fetchData = () => {
+    useEffect(() => {
+      axios.get('https://www.namava.ir/api/v5.0/search/advance?type=series&count=20&page=1&query=%D8%B3%D9%84%D8%A7%D9%85%20')
+        .then(response => {
+          setTotalContent(response.data.result.result_items[0].groups.Media.total)
+          setData(response.data.result.result_items[0].groups.Media.items)
+        })
+        .catch(error => console.log(error))
+    }, [])
+  }
+  fetchData()
   return (
     <div className="searchPage">
       <div className="inputs">
-        <FilterBox />
-        <InputBox />
+        <FiltersBox type={type} setType={setType} />
+        <InputBox input={input} setInput={setInput}/>
       </div>
-      <SearchResult />
+      <SearchedBox data={data} totalContent={totalContent} input={input} view={view} setView={setView}/>
     </div>
   )
 }

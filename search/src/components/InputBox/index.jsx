8094fe9fc05dce3b31c5c68b89506a48221adad8
Alireza <alirezaheydari7909@gmail.com>
2024-09-16 17:59:20 +0330

    add some style and PropTypes to fix bug

--- search/src/components/InputBox/index.jsx
+++ search/src/components/InputBox/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './style.css'
 import searchIcon from '../../assets/images/search.png'
-export default function InputBox () {
+InputBox.propTypes = {
+  input: PropTypes.any,
+  setInput: PropTypes.any
+}
+export default function InputBox (props) {
+  const getInput = (e) => {
+    props.setInput(e)
+  }
   return (
     <div className='inputBox'>
       <img src={searchIcon} alt="search-icon" />
-      <input type="text" placeholder='فیلم، سریال، بازیگر و ژانر'/>
+      <input
+        className='searchedText'
+        type="text"
+        placeholder='فیلم، سریال، بازیگر و ژانر'
+        onChange={(e) => getInput(e.target.value)}/>
     </div>
   )
 }

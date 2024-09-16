685428123dc869e9b19e70a11a72bc66beee5145
Alireza <alirezaheydari7909@gmail.com>
2024-09-16 18:06:58 +0330

    add component to searchBox components and add some style

--- search/src/components/SearchedBox/index.jsx
+++ search/src/components/SearchedBox/index.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
-import './style.css';
+import React from 'react'
+import PropTypes from 'prop-types'
+import './style.css'
+import EmptyState from './Result/EmptyState'
+import NotFound from './Result/NotFound'
+import SearchResult from './Result/SearchResult'
 
-export default function SearchResult() {
+SearchedBox.propTypes = {
+  data: PropTypes.any,
+  totalContent: PropTypes.any,
+  input: PropTypes.any,
+  setView: PropTypes.any,
+  view: PropTypes.any
+}
+export default function SearchedBox (props) {
+  if (props.totalContent > 0) {
+    props.setView('result')
+  } else if (props.totalContent === 0 && props.input === '') {
+    props.setView('empty')
+  } else {
+    props.setView('not-found')
+  }
   return (
-    <div>SearchResult</div>
+    <div className='search-box'>
+      { props.view === 'result' && <SearchResult data={props.data}/>}
+      { props.view === 'empty' && <EmptyState />}
+      { props.view === 'not-found' && <NotFound />}
+    </div>
   )
 }

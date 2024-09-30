64ab2b8d6137a8c87be4091089d3d557c8c942df
Alireza <alirezaheydari7909@gmail.com>
2024-09-30 15:22:02 +0330

    create react project and config eslint

---
+++ login/eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import pluginReact from "eslint-plugin-react"
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/prop-types': 'off',
+      semi: [
+        'error',
+        'never'
+      ],
+      indent: [
+        'error',
+        2,
+        {
+          SwitchCase: 1
+        }
+      ]
+    }
+  }
+]

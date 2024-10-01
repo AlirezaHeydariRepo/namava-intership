5c21cf5f29121ac8e996478c2d378fd3aa6961a2
Alireza <alirezaheydari7909@gmail.com>
2024-10-01 07:58:51 +0330

    delete some file

--- login/src/App.js
+++ login/src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
+import React from 'react'
+import Footer from './components/Footer'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='app'>
+      <LoginPage />
+      <Footer/>
     </div>
   )
 }

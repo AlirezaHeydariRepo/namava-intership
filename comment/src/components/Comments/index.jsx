0d7ff1ce937df84d9ee91093df1ff7ec92934e90
Alireza <alirezaheydari7909@gmail.com>
2024-11-11 18:06:52 +0330

    create and style Comments component

---
+++ comment/src/components/Comments/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './style.css'
+import Comment from '../Comment'
+import { Player } from '@lottiefiles/react-lottie-player'
+import loading from '../../lotties/loading.json'
+
+export default function Comments (props) {
+  const { data, fetchData, dataCount, isLoading } = props
+
+  const [page, setPage] = useState(1)
+
+  const handleMoreComments = () => {
+    const currPage = page + 1
+    fetchData(currPage)
+    setPage(currPage)
+  }
+
+  return (
+    <div className='container'>
+      {data.map((item) => {
+        return (
+          <Comment key={item.id} item={item}/>
+        )
+      })}
+      <div className={`moreComments ${dataCount < 10 ? 'inactive' : ''}`} onClick={handleMoreComments}>
+        {isLoading ? <Player className='loadingLottie' autoplay loop src={loading}/> : <span className='moreBtn'>بیشتر</span>}
+      </div>
+    </div>
+  )
+}

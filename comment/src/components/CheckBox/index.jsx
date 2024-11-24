06e05e99c250979befda4be8ac2ef999bb169ef5
Alireza <alirezaheydari7909@gmail.com>
2024-11-24 16:49:23 +0330

    reset input and checkbox after send comment

--- comment/src/components/CheckBox/index.jsx
+++ comment/src/components/CheckBox/index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './style.css'
 
 export default function CheckBox (props) {
-  const { setSpoil } = props
+  const { setSpoil, spoil } = props
   return (
     <span className='checkBox'>
-      <input className='checkBoxInput' type="checkbox" onClick={() => setSpoil(prev => !prev)}/>
+      <input className='checkBoxInput' type="checkbox" checked={spoil} onClick={() => setSpoil(prev => !prev)}/>
       <span className='checkmark'></span>
       <span className="checkBoxText">این نظر حاوی اسپویلر است و داستان فیلم را لو می‌دهد.</span>
     </span>

978d752f54d62ea9a8089edd3d4eba98241a10eb
Alireza <alirezaheydari7909@gmail.com>
2024-12-02 17:09:22 +0330

    fix slider bug and some changes in fetch data function

--- slider/src/pages/Slider/index.jsx
+++ slider/src/pages/Slider/index.jsx
@@ -1,41 +1,71 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
 import './style.css'
 import Slide from '../../components/Slide'
 
-export default function  Slider () {
+export default function Slider() {
   const [data, setData] = useState([])
+  const [indexOfCurrItem, setIndexOfCurrItem] = useState(0)
+  const timeoutRef = useRef(null)
 
-  const fetchData = () => {
-    axios.get('https://www.namava.ir/api/v2.0/medias/sliders/1316')
-      .then(response => {
-        const dataObj = response.data.result
-        setData(dataObj)
-      })
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://www.namava.ir/api/v2.0/medias/sliders/1316')
+      const dataObj = response.data.result
+      setData(dataObj)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
   }
-  console.log(data)
+  // console.log(data)
   
   useEffect(() => {
     fetchData()
   }, [])
 
-  let item = {} 
-  // eslint-disable-next-line no-unused-vars
-  const sliderFunction = setTimeout(() => {
-  }, 6000
-  )
+  useEffect(() => {
+    if (data.length > 0) {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+      timeoutRef.current = setTimeout(() => {
+        setIndexOfCurrItem(prev => prev === (data.length - 1) ? 0 : prev + 1)
+      }, 3000)
+      return () => clearTimeout(timeoutRef.current)
+    }
+  }, [indexOfCurrItem, data.length])
 
+
+  console.log(indexOfCurrItem)
+  if (!data.length) return <div>loading</div>
+  // console.log([{...data[0]}])
   
   return (
-    <div>
-      <Slide
-        key={item.id} 
-        coverLandscape={item.coverLandscape} 
-        logo={item.logoImageUrl} 
-        caption={item.caption}
-        imdb={item.imdb}
-        trailer={item.trailerVideoUrl}
-      />
+    <div className="slider-container">
+      {data.length > 0 && (
+        <>
+          {data.map((item, index) => 
+          {
+            // console.log(index === indexOfCurrItem ? 'active' : '', item)
+            return (<div
+              key={item?.id}
+              className={`${index === indexOfCurrItem ? 'activeSlide' : 'inactiveSlide'}`}
+            >
+              <Slide
+                coverLandscape={item?.coverLandscape}
+                logo={item?.logoImageUrl}
+                caption={item?.caption}
+                imdb={item?.imdb}
+                trailer={item?.trailerVideoUrl}
+                currIndex={indexOfCurrItem}
+                setIndexOfCurrItem={setIndexOfCurrItem}
+                ageRange={item?.ageRange?.value}
+                dataLength={data.length}
+              />
+            </div>)}
+          )}
+        </>
+      )}
     </div>
   )
 }

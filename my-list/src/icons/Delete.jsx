9a2205b9b2a63f0b7b06d0b47278cd98390877a5
Alireza <alirezaheydari7909@gmail.com>
2024-12-10 10:21:00 +0330

    Svg components

---
+++ my-list/src/icons/Delete.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Delete() {
+  return (
+    <svg 
+      width="40" 
+      height="40" 
+      viewBox="0 0 40 40" 
+      fill="#FFFFFF" 
+      className="Component-deleteIcon-0-1-769 Component-icon-0-1-767">
+      <path className="svg-c1" d="M31.6671 6.66667H25.8337L24.1671 5H15.8337L14.1671 6.66667H8.33374V10H31.6671M10.0004 31.6667C10.0004 32.5507 10.3516 33.3986 10.9767 34.0237C11.6018 34.6488 12.4497 35 13.3337 35H26.6671C27.5511 35 28.399 34.6488 29.0241 34.0237C29.6492 33.3986 30.0004 32.5507 30.0004 31.6667V11.6667H10.0004V31.6667Z"></path>
+    </svg>
+  )
+}
